ca8af038db8e89ab94546c24b1cc804cb1813058
caoyi <caoyia6@163.com>
2023-03-27 09:24:10 +0800

    first upload

---
+++ src/TestPackage/TestMain.java
@@ -0,0 +1,12 @@
+package TestPackage;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+public class TestMain {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(simpleDateFormat.format(System.currentTimeMillis()));
+    }
+}
