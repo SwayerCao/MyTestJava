e9a4dc9aaeae8ab3d23269176c41bec7057f0c86
caoyi <caoyia6@163.com>
2023-03-27 09:32:36 +0800

    TestPush

--- src/TestPackage/TestMain.java
+++ src/TestPackage/TestMain.java
@@ -8,5 +8,6 @@ public class TestMain {
     public static void main(String[] args) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(simpleDateFormat.format(System.currentTimeMillis()));
+        System.out.println("aaa");
     }
 }
